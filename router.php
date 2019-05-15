a158b375f9cd248ca2901a1998ed1f753b386e06
Guilhemmaxime <guilhemmaxime@gmail.com>
2019-05-15 18:35:40 +0200

    Optimise process in controller

--- router.php
+++ router.php
@@ -11,7 +11,6 @@ class Router
 
     // Default properties
     protected $page           = null;
-    protected $rubric         = null;
     protected $controller     = self::DEFAULT_CONTROLLER;
     protected $action         = self::DEFAULT_ACTION;
 
@@ -39,11 +38,8 @@ class Router
         // Cuts this page value with the exclamation point
         $access = explode('!', $this->page);
 
-        // Attributes the first access string to this rubric
-        $this->rubric = $access[0];
-
-        // Attributes the value of this rubric to this controller
-        $this->controller = $this->rubric;
+        // Attributes the first access string to this controller
+        $this->controller = $access[0];
 
         // If set, attributes the second access string to the current action method
         // if not set, attributes the string index
