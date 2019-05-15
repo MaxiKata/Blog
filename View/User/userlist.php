84b9b3ca3a4e44a45eaf17a280791ef49989c176
Guilhemmaxime <guilhemmaxime@gmail.com>
2019-05-15 11:46:37 +0200

    Add Views User

---
+++ View/User/userlist.php
@@ -0,0 +1,27 @@
+<?php
+
+$title = "Liste des utilisateurs";
+require_once('View/layout.php'); ?>
+
+<body>
+    <?php require ('View/header.php'); ?>
+
+    <h1><?= $title ?></h1>
+
+    <?php
+    foreach($users as $user)
+    {
+        ?>
+        <div class="news">
+            <h3>
+                <?= htmlspecialchars($user['nickname']); ?>
+            </h3>
+
+            <p>
+                <em><a href="index.php?userid=<?=$user['id'] ?>&access=user!profil">Consulter</a></em>
+            </p>
+        </div>
+        <?php
+    }
+    ?>
+</body>
---
+++ View/User/userprofil.php
@@ -0,0 +1,17 @@
+<?php
+
+$title = "Consulter le profil";
+require_once('View/layout.php'); ?>
+
+<body>
+    <?php require ('View/header.php'); ?>
+
+    <h1><?= $title ?> de <?= htmlspecialchars($useredit->getUsername(), ENT_QUOTES) ?></h1>
+    <div class="news">
+        <p><a href="index.php?access=user!list">Retour à la liste des utilisateurs</a></p>
+        <h3>
+            Nom d'utilisateur <?= htmlspecialchars($useredit->getUsername(), ENT_QUOTES); ?>
+        </h3>
+    </div>
+
+</body>
