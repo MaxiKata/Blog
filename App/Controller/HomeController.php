78a0fc34287d7f2f0b571f9b3a7319b3faa96f50
Guilhemmaxime <guilhemmaxime@gmail.com>
2019-05-24 15:52:08 +0200

    Solve bug on Alert update
    - [x] Add Alert function missing

--- App/Controller/HomeController.php
+++ App/Controller/HomeController.php
@@ -3,6 +3,9 @@
 namespace Blog\App\Controller;
 
 
+use Blog\App\Alerts\Error;
+use Blog\App\Alerts\Success;
+
 class HomeController
 {
     public function indexAction()
@@ -10,5 +13,47 @@ class HomeController
         $alert = $this->getAlert();
 
         require_once('../View/home.php');
+    }
+    private function getAlert()
+    {
+        if(isset($_GET['success']) || isset($_GET['error'])){
+            if(isset($_GET['success'])){
+                $success = new Success();
+                $function = htmlspecialchars($_GET['success'], ENT_QUOTES);
+
+                if(method_exists($success, $function) == true){
+                    $successAlert = $success->$function();
+
+                    return $successAlert;
+                }
+                else{
+                    $error = new Error();
+                    $function = "notAllowed";
+                    $errorAlert = $error->$function();
+
+                    return $errorAlert;
+                }
+
+            }
+            else{
+                $error = new Error();
+                $function = htmlspecialchars($_GET['error'], ENT_QUOTES);
+
+                if(method_exists($error, $function) == true){
+                    $errorAlert = $error->$function();
+                    return $errorAlert;
+                }
+                else{
+                    $function = "notAllowed";
+                    $errorAlert = $error->$function();
+
+                    return $errorAlert;
+                }
+
+            }
+
+
+
+        }
     }
 }
