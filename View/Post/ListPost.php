6e4e9682602f64b6b76391de0c020ceec7a4c605
Guilhemmaxime <guilhemmaxime@gmail.com>
2019-05-29 10:40:04 +0200

    Update views with css

--- View/Post/ListPost.php
+++ View/Post/ListPost.php
@@ -6,15 +6,16 @@ require_once('../View/layout.php'); ?>
 <body>
     <?php require ('../View/header.php'); ?>
 
-    <h1><?= $title ?></h1>
+    <h1 class="mt-3"><?= $title ?><?php if(isset($_GET['category'])){ ?> / <?= $categories[0]['category'];} ?></h1>
     <?= $alert; ?>
-    <div class="d-flex">
-        <div>
+    <div class="d-flex article-page">
+        <div class="article-list">
             <?php
             foreach($posts as $data)
             {
+                $color = $data->getColor();
                 ?>
-                <div class="news">
+                <div class="article" style="border-color: <?= $data->getColor()?>" onmouseover="this.style.backgroundColor='<?= $color?>'; this.style.borderColor='<?= $color?>';" onmouseout="this.style.backgroundColor=''; this.style.borderColor='<?= $color?>';">
                     <h3>
                         <?= $data->getTitle(); ?>
                         <em>le <?= $data->getDateUpdate(); ?></em>
@@ -23,26 +24,45 @@ require_once('../View/layout.php'); ?>
                     <p>
                         <?= nl2br($data->getContent()); ?>
                         <br>
-                        <em><a href="index.php?id=<?=$data->getId() ?>&access=blog!read">Consulter</a></em>
                     </p>
+                    <span class="article-button"><a href="index.php?id=<?=$data->getId() ?>&access=blog!read"><button>Consulter</button></a></span>
                 </div>
                 <?php
-            }
-            for($i=1; $i<=$nbPage; $i++){
-                if($i==$page){
-                    echo " $i ";
+            } ?>
+            <div class="text-center h4">
+                <?php for($i=1; $i<=$nbPage; $i++){
+                    if($i==$page){
+                        echo " $i ";
+                    }
+                    else{
+                        echo " <a href=\"index.php?access=blog&p=$i\">$i</a> ";
+                    }
                 }
-                else{
-                    echo " <a href=\"index.php?access=blog&p=$i\">$i</a> ";
-                }
-            }
-            ?>
+                ?>
+            </div>
         </div>
-       <ul>
-           <?php foreach($categories as $category){ ?>
-               <li><a href="index.php?access=blog!category&category=<?= $category['category'] ?>"><?= $category['category']  ?> <?= $category['nbPost']  ?> article(s)</a></li>
-           <?php } ?>
-       </ul>
+        <aside>
+            <h2>Catégories</h2>
+            <ul>
+                <?php foreach($categories as $category) {
+                    if(isset($_GET['category']) && $category['category'] == $_GET['category']){ ?>
+                        <li class="choosen"style="background-color: <?= $category['color'] ?>; border-color: <?= $category['color']?>">
+                            <?= $category['category'] ?> <?=$category['nbPost'] ?> article(s)
+                        </li>
+                    <?php }
+                    else{
+                        $color = $category['color']?>
+                        <a href="index.php?access=blog!category&category=<?= $category['category'] ?>">
+                            <li style="border-color: <?= $color ?>" onmouseover="this.style.backgroundColor='<?= $color?>'; this.style.borderColor='<?= $color?>';" onmouseout="this.style.backgroundColor=''; this.style.borderColor='<?= $color?>';">
+                                <?= $category['category'] ?> <?=$category['nbPost'] ?> article(s)
+                            </li>
+                        </a>
+                    <?php } ?>
+                <?php } ?>
+            </ul>
+        </aside>
     </div>
+
+
 
 </body>
--- View/Post/PostView.php
+++ View/Post/PostView.php
@@ -15,14 +15,16 @@ require_once('../View/layout.php'); ?>
 
     <h1><?= $title ?></h1>
     <?= $alert; ?>
-    <p><a href="<?= $directory ?>/index.php?access=blog">Retour à la liste des billets</a></p>
-    <?php
+    <div class="linksMenu">
+        <p><a href="<?= $directory ?>/index.php?access=blog">Retour à la liste des billets</a></p>
+        <?php
         if(isset($_SESSION['Statut_id']) && $_SESSION['Statut_id'] == 2){ ?>
             <p><a href="<?=$directory ?>/index.php?id=<?= $p_id ?>&access=blog!modifypost">Modifier</a></p>
         <?php }
-    ?>
+        ?>
+    </div>
 
-    <div class="news">
+    <article class="news">
         <h3>
             Par <?= $p_author;?>
             <em>le <?= $p_dateUpd ?></em>
@@ -31,56 +33,62 @@ require_once('../View/layout.php'); ?>
         <p>
             <?= nl2br($p_content); ?>
         </p>
-    </div>
-    <h2>Commentaires</h2>
-    <?php
-    if(isset($_SESSION['Statut_id'])){
-        if($_SESSION['Statut_id'] == 1 || $_SESSION['Statut_id'] == 2 ){ ?>
-            <form action="<?= $directory ?>/index.php?access=comment!publish" method="post">
-                <label for="comment">Commentaire</label>
-                <textarea type="text" id="comment" name="comment"></textarea>
-                <button type="submit" name="publish">Publier</button>
-                <input type="text" id="id" name="id" hidden value="<?= nl2br($p_id); ?>">
-            </form>
-        <?php }
-    }
-    ?>
-
-    <?php if(empty($comments)){
-        echo 'Soyez le premier à poster un commentaire';
-    }
-    else{
-        foreach($comments as $com)
-        {
-            $com_username = $com->getUUsername();
-            $com_edit_username = $com->getUsUsername();
-            $com_content = $com->getContent();
-            $com_dateUpdate = $com->getDateComUpdate();
-            $com_statut = $com->getStatutId();
-            $com_id = $com->getId();
-            $com_uid = $com->getUserId();
-            ?>
-            <div class="news">
-                <h3>
-                    Par <?= $com_username; ?>
-                    <em>le <?= $com_dateUpdate; ?></em>
-                    <?php if($com_statut == 6 && $com_username !== $com_edit_username) {
-                        echo 'Modifié par '. $com_edit_username;
-                    } ?>
-                </h3>
-
-                <p><?= nl2br($com_content); ?></p>
-                <?php
-                if(isset($_SESSION['Statut_id']) && $_SESSION['Statut_id'] == 2){ ?>
-                    <p><a href="<?=$directory ?>/index.php?access=comment!modify&id=<?= $p_id ?>&commentid=<?= $com_id ?>">Modifier</a></p>
-                <?php }
-                elseif(isset($_SESSION['Statut_id']) && $_SESSION['id'] == $com_uid){ ?>
-                    <p><a href="<?=$directory ?>/index.php?access=comment!modify&id=<?= $p_id ?>&commentid=<?= $com_id ?>">Modifier</a></p>
-                <?php }
-                ?>
-            </div>
-            <?php
+    </article>
+    <div class="comment">
+        <h2>Commentaires</h2>
+        <?php
+        if(isset($_SESSION['Statut_id'])){
+            if($_SESSION['Statut_id'] == 1 || $_SESSION['Statut_id'] == 2 ){ ?>
+                <form class="post-comment" action="<?= $directory ?>/index.php?access=comment!publish" method="post">
+                    <label for="comment" hidden >Commentaire</label>
+                    <textarea type="text" id="comment" name="comment" placeholder="N'hésitez pas à laisser un petit message"></textarea>
+                    <button type="submit" name="publish">Publier</button>
+                    <input type="text" id="id" name="id" hidden value="<?= nl2br($p_id); ?>">
+                </form>
+            <?php }
         }
-    } ?>
+        else{ ?>
+            <a href="<?= $directory ?>/index.php?access=user"><button>Connectez vous vite pour répondre</button></a>
+        <?php }
+        ?>
+        <br>
+        <?php if(empty($comments)){
+            echo 'Soyez le premier à poster un commentaire';
+        }
+        else{
+            foreach($comments as $com)
+            {
+                $com_username = $com->getUUsername();
+                $com_edit_username = $com->getUsUsername();
+                $com_content = $com->getContent();
+                $com_dateUpdate = $com->getDateComUpdate();
+                $com_statut = $com->getStatutId();
+                $com_id = $com->getId();
+                $com_uid = $com->getUserId();
+                ?>
+                <div class="news">
+                    <h3>
+                        Par <?= $com_username; ?>
+                        <em>le <?= $com_dateUpdate; ?></em>
+                        <?php if($com_statut == 6 && $com_username !== $com_edit_username) {
+                            echo 'Modifié par '. $com_edit_username;
+                        } ?>
+                    </h3>
+
+                    <p><?= nl2br($com_content); ?></p>
+                    <?php
+                    if(isset($_SESSION['Statut_id']) && $_SESSION['Statut_id'] == 2){ ?>
+                        <p><a href="<?=$directory ?>/index.php?access=comment!modify&id=<?= $p_id ?>&commentid=<?= $com_id ?>">Modifier</a></p>
+                    <?php }
+                    elseif(isset($_SESSION['Statut_id']) && $_SESSION['id'] == $com_uid){ ?>
+                        <p><a href="<?=$directory ?>/index.php?access=comment!modify&id=<?= $p_id ?>&commentid=<?= $com_id ?>">Modifier</a></p>
+                    <?php }
+                    ?>
+                </div>
+                <?php
+            }
+        } ?>
+    </div>
+
 
 </body>
