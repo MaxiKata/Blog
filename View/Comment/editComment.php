64b3713cbf2a788ea7102e1769d9f3ea915abdaa
Guilhemmaxime <guilhemmaxime@gmail.com>
2019-06-05 17:02:32 +0200

    Fix error escaping function
    All output should be run through an escaping function

--- View/Comment/editComment.php
+++ View/Comment/editComment.php
@@ -22,7 +22,7 @@ if(isset($sessionStatut)){
         <body>
         <?php require ('../View/header.php'); ?>
 
-        <p class="mt-3 comment"><a href="<?=  $directory ?>/index.php?id=<?= filter_var($p_id, FILTER_SANITIZE_STRING); ?>&access=blog!read">Retour à l'article</a></p>
+        <p class="mt-3 comment"><a href="<?=  $directory ?>/index.php?id=<?php esc_attr($p_id); ?>&access=blog!read">Retour à l'article</a></p>
         <?= $alert; ?>
         <form class="comment" action="<?= $directory ?>/index.php?access=comment!update" method="post">
             <div class="d-grid">
