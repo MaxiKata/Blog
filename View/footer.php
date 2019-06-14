c53fc3ecf316754ee819a6e5fcb3c7a6b5a4c5e3
Guilhemmaxime <guilhemmaxime@gmail.com>
2019-06-14 16:53:03 +0200

    Create Footer
    [ x ] Add social media links
    [ x ] Add link to download CV in pdf

---
+++ View/footer.php
@@ -0,0 +1,12 @@
+<footer class="container p-3">
+    <div class="row">
+        <div class="col-4 mx-auto text-center">
+            <a class="col-4" href="https://www.linkedin.com/in/maxime-guilhem/"><img class="social-logo" src="<?= $directory ?>/Public/img/linkedin.svg" alt="LinkedIn logo" /></a>
+            <a class="col-4" href="https://github.com/MaxiKata"><img class="social-logo" src="<?= $directory ?>/Public/img/github.svg" alt="GitHub logo" /></a>
+            <a class="col-4" href="https://twitter.com/GuilhemMaxime"><img class="social-logo" src="<?= $directory ?>/Public/img/twitter.svg" alt="Twitter logo" /></a>
+        </div>
+        <div class="col-6 text-center justify-content-center align-self-center">
+            <a class="" href="<?= $directory ?>/Public/CV_Maxime.pdf" download><button class="btn btn-primary">Télécharger mon CV en PDF :)</button> </a>
+        </div>
+    </div>
+</footer>
